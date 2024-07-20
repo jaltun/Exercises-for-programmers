69e77b0ffc0b2bf40fb485292225dce1841a07b3
julia <111418242+jaltun@users.noreply.github.com>
2024-07-20 22:25:02 +0200

    user has to give an input

--- Task2-CountingTheNumbersOfCharacters/Program.cs
+++ Task2-CountingTheNumbersOfCharacters/Program.cs
@@ -1,10 +1,11 @@
 ﻿string inputString = "";
-int stringLength = inputString.Length;
+int stringLength = 0;
 do
 {
     Console.Write("What is the input string? ");
     inputString = Console.ReadLine();
 
-    //stringLength = inputString.Length;
+    stringLength = inputString.Length;
 } while (stringLength == 0);
+
 Console.WriteLine($"{inputString} has {stringLength} characters.");

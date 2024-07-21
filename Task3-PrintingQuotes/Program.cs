683545cfedcb4324b94206bc3eab189d2f137a04
julia <111418242+jaltun@users.noreply.github.com>
2024-07-21 17:22:24 +0200

    finish task 3

---
+++ Task3-PrintingQuotes/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("What is the quote? ");
+string quote = Console.ReadLine();
+
+Console.Write("Who said it? ");
+string author = Console.ReadLine();
+
+Console.WriteLine($"{author} says, \"{quote}\"");

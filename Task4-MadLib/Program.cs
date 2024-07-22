1a56a83dc2396c7a8ca9896ac89c9126fa1f437f
julia <111418242+jaltun@users.noreply.github.com>
2024-07-22 20:59:00 +0200

    finish mad lib

---
+++ Task4-MadLib/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter a noun: ");
+string noun = Console.ReadLine();
+
+Console.Write("Enter a verb: ");
+string verb = Console.ReadLine();
+
+Console.Write("Enter an adjective: ");
+string adjective = Console.ReadLine();
+
+Console.Write("Enter an adverb: ");
+string adverb = Console.ReadLine();
+
+string madLibStory = $"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!";
+Console.WriteLine(madLibStory);

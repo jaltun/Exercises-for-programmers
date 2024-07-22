3190569210010bb8cbbbd31e5dba97377244e490
julia <111418242+jaltun@users.noreply.github.com>
2024-07-22 21:07:58 +0200

    finish simple math

---
+++ Task5-SimpleMath/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("What is the first number? ");
+string firstNumberString = Console.ReadLine();
+int firstNumber = int.Parse(firstNumberString);
+
+Console.Write("What is the second number? ");
+string secondNumberString = Console.ReadLine();
+int secondNumber = int.Parse(secondNumberString);
+
+int sum = firstNumber + secondNumber;
+int difference = firstNumber - secondNumber;
+int product = firstNumber * secondNumber;
+int quotient = firstNumber / secondNumber;
+
+Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+Console.WriteLine($"{firstNumber} - {secondNumber} = {difference}");
+Console.WriteLine($"{firstNumber} * {secondNumber} = {product}");
+Console.WriteLine($"{firstNumber} / {secondNumber} = {quotient}");

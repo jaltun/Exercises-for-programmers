36777edfc08cd84dfb9b8c42df5c47eb5c5fa5fe
julia <111418242+jaltun@users.noreply.github.com>
2024-07-23 22:42:23 +0200

    finish exercise

--- Task6-RetirementCalculator/Program.cs
+++ Task6-RetirementCalculator/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    Console.Write("What is your current age? ");
+    int currentAge = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("At what age would you like to retire? ");
+    int retirementAge = Convert.ToInt32(Console.ReadLine());
+
+    int currentYear = DateTime.Now.Year;
+
+    int yearsLeft = retirementAge - currentAge;
+
+    int retirementYear = currentYear + yearsLeft;
+
+    Console.WriteLine($"You have {yearsLeft} years left until you can retire.");
+    Console.WriteLine($"It's {currentYear}, so you can retire in {retirementYear}.");
